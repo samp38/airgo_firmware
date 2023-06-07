878496979bb6a666eab41763523ca73842d98b1a
sheidman <samuel.heidmann@inria.fr>
2023-06-07 11:23:09 +0200

    forgot ifndef

--- include/bsp.h
+++ include/bsp.h
@@ -1,6 +1,11 @@
+#ifndef __BSP_H__
+#define __BSP_H__
+
 // PWM outputs for motor via H-Bridge
 #define MOTOR_OUT_1 9
 #define MOTOR_OUT_2 10
 
 // Push Button input (must allow EXT INT to wake up from sleep)
 #define PUSH_BUTTON_IN 2
+
+#endif
